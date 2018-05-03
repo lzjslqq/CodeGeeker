173f5240ab29accdeaa8e64421271c191413a974
lzjslqq <chief.king@qq.com>
2018-05-03 09:53:19 +0800

    替换切图

--- CodeGeeker/pages/user/upload/upload.js
+++ CodeGeeker/pages/user/upload/upload.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
      data: {
-          matchArray: ['美国', '中国', '巴西', '日本', '韩国', '阿尔巴尼亚', '朝鲜'],
+          matchArray: ['美国VS德国', '中国VS巴西', '巴西VS阿根廷', '日本VS朝鲜', '韩国VS智利', '阿尔巴尼亚VS安哥拉', '伊朗VS泰国'],
           categoryArray: ['球迷风采', '梅西', 'C罗', '内马尔', '小罗', '阿根廷','其他'],
           selectedMatchId: null,
           selectedcategoryId:null,
