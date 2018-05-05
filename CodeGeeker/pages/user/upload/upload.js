35705f098aa12015317d4c748bbbb475e67170ca
lzjslqq <chief.king@qq.com>
2018-05-05 20:51:45 +0800

    添加upload逻辑

--- CodeGeeker/pages/user/upload/upload.js
+++ CodeGeeker/pages/user/upload/upload.js
@@ -5,9 +5,10 @@ const app = getApp()
 Page({
      data: {
           matchArray: ['美国VS德国', '中国VS巴西', '巴西VS阿根廷', '日本VS朝鲜', '韩国VS智利', '阿尔巴尼亚VS安哥拉', '伊朗VS泰国'],
-          categoryArray: ['球迷风采', '梅西', 'C罗', '内马尔', '小罗', '阿根廷','其他'],
+          categoryArray: ['球迷风采', '梅西', 'C罗', '内马尔', '小罗', '阿根廷', '其他'],
           selectedMatchId: null,
-          selectedcategoryId:null,
+          selectedcategoryId: null,
+          tempPhotoPathList: [],
      },
 
      onLoad: function () {
@@ -16,16 +17,36 @@ Page({
      onShow: function () { },
      onReady: function () { },
      bindMatchChange: function (e) {
+
           console.log('picker发送选择改变，携带值为', e.detail.value)
-          this.setData({
-               selectedMatchId: e.detail.value
-          })
+          this.setData({ selectedMatchId: e.detail.value })
      },
      bindCategoryChange: function (e) {
+
           console.log('picker发送选择改变，携带值为', e.detail.value)
-          this.setData({
-               selectedcategoryId: e.detail.value
-          })
+          this.setData({ selectedcategoryId: e.detail.value })
      },
+     bindChooseImage(e) {
+
+          promisedApi.image.chooseImage()
+               .then((res) => {
+                    if (res.tempFilePaths && res.tempFilePaths.length > 0) {
+
+                         this.setData({ tempPhotoPathList: this.data.tempPhotoPathList.concat(res.tempFilePaths) });
+                    }
+
+               });
+     },
+     bindDeleteImage(e) {
+
+          let index = e.currentTarget.dataset.index;
+          this.data.tempPhotoPathList.splice(index, 1);
+          this.setData({ tempPhotoPathList: this.data.tempPhotoPathList });
+     },
+     bindPublish(e) {
+          if (!this.data.selectedMatchId || !this.data.selectedcategoryId || this.data.tempPhotoPathList.length == 0) {
+               promisedApi.ui.showToast({ title: '还未上传图片', icon: 'none', duration: 1000 });
+          }
+     }
 
 })
